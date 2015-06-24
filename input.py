3fb0d838f74b064fa7c9209cc7ade3541bc65e63
hitanjan <hitanjan@gmail.com>
2015-06-25 02:00:56 +0530

    Updating dataset to save url data in text files that can be loaded by scikit's dataset loading utilities. Refine logic to pre-process csv data wrt loading company names from csv file

--- input.py
+++ input.py
@@ -3,9 +3,10 @@ __author__ = 'hitanjan'
 import csv
 import urllib2
 from bs4 import BeautifulSoup
-import numpy
+import shutil
 import os
-import shelve
+from urlparse import urlparse
+import sklearn.datasets
 
 
 
@@ -41,25 +42,34 @@ class model():
             self.text = self.text.encode("utf-8")
         if(self.text == ""):
             raise BaseException("No data found in URL - %s" % self.potential_url)
-        self.descendant_links = self.potential_url_object.get_embedded_links()  # TODO - Use this feature later and Incorporate link structure too ??
+        self.descendant_links = self.potential_url_object.get_embedded_links()  # TODO - Use this feature later and Incorporate link structure too ?? Use a separate feature_set dir in dataset object
         del self.potential_url_object
 
         # TODO Pre-process ? Convert to lower ? Trim spaces ? special chars ?
 
 
 class dataset():
+    __positive__ = "positive"
+    __negative__ = "negative"
+    __feature_set_1__ = "url_contents"
+    __cache_dir__ = 'dataset_cache'
+    __feature_set1_dir_positive__ = os.path.join(__cache_dir__,__feature_set_1__,__positive__)
+    __feature_set1_dir_negative__ = os.path.join(__cache_dir__,__feature_set_1__,__negative__)
+    __feature_set1_dir__ = os.path.join(__cache_dir__, __feature_set_1__)
+
     def __init__(self, positive_csv_data, negative_csv_data):
         self.positive_file = positive_csv_data
         self.negative_file = negative_csv_data
-        self.__cache_dir__ = 'dataset_cache'
+        self.__create_cache_dir()
 
-
-    def _load_data_from_csv(self, file):
+    def __create_cache_dir(self):
         if(not os.path.exists(self.__cache_dir__)):
-            print "Loading data for the first time... This may take some time.."
+            print "Building cache... This may take some time.."
             os.makedirs(self.__cache_dir__)
+            os.makedirs(self.__feature_set1_dir_positive__)
+            os.makedirs(self.__feature_set1_dir_negative__)
 
-        cache = shelve.open(os.path.join(self.__cache_dir__, os.path.basename(file)))
+    def _build_dataset_cache_from_csv_file(self, file, classifier_dir):
 
         with open(file, 'rU') as csv_file:
                 reader = csv.reader(csv_file, delimiter=',')
@@ -67,14 +77,18 @@ class dataset():
 
                 for row in reader:
 
-                    # TODO : Deal with comma separated 'Inc.' and other occurrences in csv file. Remove the index of the term. Refine Logic
-                    for (i,index) in [('Inc.' == element.strip(),row.index(element)) for element in row]:
-                        if(i == True):
-                            del row[index]
+                    # Deal with comma separated 'Inc.' and other such occurrences in csv file. Remove the index of the term. Deals with rogue commas in a csv row
+                    if(len(row) > 2):
+                        element = row[1]
+                        valid_url = urlparse(element)
+                        if(not valid_url.netloc):
+                            # Not a url, a plain string
+                            row[0] += element
+                            del row[1]
 
                     input_model = model(row[1], row[0])
-                    if(cache.has_key(input_model.company_name)):
-                        text = cache[input_model.company_name]
+                    if(os.path.exists(os.path.join(classifier_dir, input_model.company_name))):
+                        continue
                     else:
                         try:
                             input_model.load_raw_data()
@@ -82,48 +96,33 @@ class dataset():
                             print("Error in loading dataset from model(potential_url=%s, company_name=%s) found in file : %s..Skipping") % (input_model.potential_url, input_model.company_name, file)
                             print ex
                             continue
-                        text = cache[input_model.company_name] = input_model.text
-
-                    data += [(input_model.company_name, text)]
-
-
-        cache.close()
-        return data
+                        text = input_model.text
+                        text_file = open(os.path.join(classifier_dir, input_model.company_name), 'w')
+                        text_file.write(text)
+                        text_file.close()
 
 
     def load(self):
 
-        positive_data = self._load_data_from_csv(self.positive_file)
-        data_positive = numpy.array(positive_data)
-        class_positive = numpy.array([1 for r in range(len(positive_data))])
+        self.build_data_set_cache()
 
+        # TODO utf-8 ??
+        dataset = sklearn.datasets.load_files(self.__feature_set1_dir__)
 
-        negative_data = self._load_data_from_csv(self.negative_file)
-        data_negative = numpy.array(negative_data)
-        class_negative = numpy.array([0 for r in range(len(negative_data))])
-
-
-
-        # Equal sizes of positive and negative training data
-        # TODO - Shrink one list on basis of the other to make them equal before shuffle
-        if (len(class_negative) != len(class_positive)):
-            raise Exception("Positive and Negative Dataset Sizes don't match")
-
-        data = numpy.concatenate((data_positive, data_negative))
-        classification = numpy.concatenate((class_positive, class_negative))
-
-        # shuffle
-        shuffle_binder = numpy.c_[data.reshape(len(data), -1), classification.reshape(len(classification), -1)]
-        newdata = shuffle_binder[:, :data.size // len(data)].reshape(data.shape)
-        newclassification = shuffle_binder[:, data.size // len(data):].reshape(classification.shape)
-        numpy.random.shuffle(shuffle_binder)
-
-
-        dataset.data = newdata
-        dataset.classification = newclassification
         return dataset
 
 
+    def rebuild_data_set_cache(self):
+        if(os.path.exists(self.__cache_dir__)):
+            shutil.rmtree(self.__cache_dir__)
+
+
+        self.build_data_set_cache()
+
+
+    def build_data_set_cache(self):
+        self._build_dataset_cache_from_csv_file(self.positive_file, self.__feature_set1_dir_positive__)
+        self._build_dataset_cache_from_csv_file(self.negative_file, self.__feature_set1_dir_negative__)
 
 
 
